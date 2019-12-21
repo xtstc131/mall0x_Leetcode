bfa1ad800c6a480b4d8caa76bc57fae53baf9d66
malloxia <mallxia@163.com>
2019-12-21 03:49:36 -0500

    2019-12-21

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int a = pow(2, 25) - 1;
+    float b;
+    b = a;
+    printf("a = %d\n", a);
+    printf("b = %lf\n", b);
+}
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    int a = 0;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
