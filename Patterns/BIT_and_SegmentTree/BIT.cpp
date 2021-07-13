68fb3a7737ee4576cc7f81a930eb483ffb4e16c8
Tian Xia <txia@uottawa.ca>
2021-07-12 22:29:09 -0400

    Format files

--- Patterns/BIT_and_SegmentTree/BIT.cpp
+++ Patterns/BIT_and_SegmentTree/BIT.cpp
@@ -10,8 +10,8 @@ public:
     BIT(int n)
     {
         _size = n;
-        tree.resize(n+1);
-        for(int i = 0 ;i <= n ; ++i)
+        tree.resize(n + 1);
+        for (int i = 0; i <= n; ++i)
             tree[i] = 0;
     }
     int lowbit(int x)
@@ -20,7 +20,7 @@ public:
     }
     void update(int index, int delta)
     {
-        while(index <= _size)
+        while (index <= _size)
         {
             tree[index] += delta;
             index += lowbit(index);
@@ -30,12 +30,11 @@ public:
     int query(int index)
     {
         int res = 0;
-        while(index > 0)
+        while (index > 0)
         {
             res += tree[index];
             index -= lowbit(index);
         }
         return res;
     }
-
 };
--- Patterns/BIT_and_SegmentTree/segment_tree.cpp
+++ Patterns/BIT_and_SegmentTree/segment_tree.cpp
@@ -1,59 +1,64 @@
 #include "header.hpp"
-class SegmentTree {
+class SegmentTree
+{
 public:
-    SegmentTree(vector<int>& nums) {
+    SegmentTree(vector<int> &nums)
+    {
         n = nums.size();
         tree.resize(n * 2);
         buildTree(nums);
     }
-    
-    void update(int index, int val) {
+
+    void update(int index, int val)
+    {
         index += n;
         tree[index] = val;
-        while(index)
+        while (index)
         {
             int left = index;
             int right = index;
-            if(index % 2 == 0)
+            if (index % 2 == 0)
                 right = index + 1;
             else
                 left = index - 1;
-            tree[index/2] = tree[left] + tree[right];
-            index/=2;
+            tree[index / 2] = tree[left] + tree[right];
+            index /= 2;
         }
     }
-    
-    int sumRange(int left, int right) {
+
+    int sumRange(int left, int right)
+    {
         left += n;
         right += n;
         int sum = 0;
-        while(left <= right)
+        while (left <= right)
         {
-            if(left % 2 == 1)
+            if (left % 2 == 1)
             {
                 sum += tree[left];
                 left++;
             }
-            if(right % 2 == 0)
+            if (right % 2 == 0)
             {
                 sum += tree[right];
                 right--;
             }
             right /= 2;
-            left /=2;
+            left /= 2;
         }
         return sum;
     }
+
 private:
     vector<int> tree;
     int n;
-    void buildTree(vector<int>& nums)
+    void buildTree(vector<int> &nums)
     {
-        for(int i = n, j = 0; i < n*2; ++i, ++j)
+        for (int i = n, j = 0; i < n * 2; ++i, ++j)
         {
             tree[i] = nums[j];
         }
-        for(int i = n-1 ; i > 0; --i)
+        for (int i = n - 1; i > 0; --i)
         {
             tree[i] = tree[i * 2] + tree[i * 2 + 1];
         }
